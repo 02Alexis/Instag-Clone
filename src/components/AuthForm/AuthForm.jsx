cafbd758caa21b51b5d65770df0c56c32543ec03
02Alexis <m_chael1012@hotmail.com>
2024-03-02 11:37:35 -0500

    diseño de la pagina de autenticacion

---
+++ src/components/AuthForm/AuthForm.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+const AuthForm = () => {
+  const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true);
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleAuth = () => {
+    if (!inputs.email || !inputs.password) {
+      alert("Por favor llene todos los campos");
+      return;
+    }
+    navigate("/");
+  };
+
+  return (
+    <>
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <VStack spacing={4}>
+          <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+
+          <Input
+            placeholder="Email"
+            fontSize={14}
+            type="email"
+            value={setInputs.email}
+            onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+          />
+          <Input
+            placeholder="Password"
+            fontSize={14}
+            type="password"
+            value={setInputs.password}
+            onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+          />
+
+          {isLogin ? (
+            <Input
+              placeholder="Confirmar Password"
+              fontSize={14}
+              type="password"
+              value={setInputs.confirmPassword}
+              onChange={(e) =>
+                setInputs({ ...inputs, confirmPassword: e.target.value })
+              }
+            />
+          ) : null}
+
+          <Button
+            w={"full"}
+            colorScheme="blue"
+            size={"sm"}
+            fontSize={14}
+            onClick={handleAuth}
+          >
+            {isLogin ? "Log in" : "Sing up"}
+          </Button>
+
+          {/* {isLogin ? <Login /> : <Signup />} */}
+
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            my={4}
+            gap={1}
+            w={"full"}
+          >
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+            <Text mx={1} color={"white"}>
+              OR
+            </Text>
+            <Box flex={2} h={"1px"} bg={"gray.400"} />
+          </Flex>
+        </VStack>
+      </Box>
+
+      <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+        <Flex alignItems={"center"} justifyContent={"center"}>
+          <Box mx={2} fontSize={14}>
+            {isLogin ? "¿No tienes una cuenta?" : "¿Ya tienes una cuenta?"}
+          </Box>
+          <Box
+            onClick={() => setIsLogin(!isLogin)}
+            color={"blue.500"}
+            cursor={"pointer"}
+          >
+            {isLogin ? "Sign up" : "Log in"}
+          </Box>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default AuthForm;
