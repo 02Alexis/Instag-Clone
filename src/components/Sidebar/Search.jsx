448f86701566868a71ef9c2113be10260c288421
02Alexis <m_chael1012@hotmail.com>
2024-03-04 17:08:52 -0500

    buscador por perfil de usuario

--- src/components/Sidebar/Search.jsx
+++ src/components/Sidebar/Search.jsx
@@ -1,29 +1,90 @@
-import { Box, Flex, Tooltip } from "@chakra-ui/react";
+import { useRef } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  FormControl,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Tooltip,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { SearchLogo } from "../../assets/constants";
+import useSearchUser from "../../hooks/useSearchUser";
+import SuggestedUser from "../SuggestedUsers/SuggestedUser";
 
 const Search = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const searchRef = useRef(null);
+  const { user, isLoading, getUserProfile, setUser } = useSearchUser();
+
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    getUserProfile(searchRef.current.value);
+  };
+
+  console.log(user);
+
   return (
-    <Tooltip
-      hasArrow
-      label={"Search"}
-      placement="right"
-      ml={1}
-      openDelay={500}
-      display={{ base: "block", md: "none" }}
-    >
-      <Flex
-        alignItems={"center"}
-        gap={4}
-        _hover={{ bg: "whiteAlpha.400" }}
-        borderRadius={6}
-        p={2}
-        w={{ base: 10, md: "full" }}
-        justifyContent={{ base: "center", md: "flex-start" }}
+    <>
+      <Tooltip
+        hasArrow
+        label={"Search"}
+        placement="right"
+        ml={1}
+        openDelay={500}
+        display={{ base: "block", md: "none" }}
       >
-        <SearchLogo />
-        <Box display={{ base: "none", md: "block" }}>Buscar</Box>
-      </Flex>
-    </Tooltip>
+        <Flex
+          alignItems={"center"}
+          gap={4}
+          _hover={{ bg: "whiteAlpha.400" }}
+          borderRadius={6}
+          p={2}
+          w={{ base: 10, md: "full" }}
+          justifyContent={{ base: "center", md: "flex-start" }}
+          onClick={onOpen}
+        >
+          <SearchLogo />
+          <Box display={{ base: "none", md: "block" }}>Buscar</Box>
+        </Flex>
+      </Tooltip>
+
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInLeft">
+        <ModalOverlay />
+        <ModalContent bg={"black"} border={"1px solid gray"} maxW={"400px"}>
+          <ModalHeader>Buscar usuario</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSearchUser}>
+              <FormControl>
+                <FormLabel>Nombre de Usuario</FormLabel>
+                <Input placeholder="Alexis" ref={searchRef} />
+              </FormControl>
+
+              <Flex w={"full"} justifyContent={"flex-end"}>
+                <Button
+                  type="submit"
+                  ml={"auto"}
+                  size={"sm"}
+                  my={4}
+                  isLoading={isLoading}
+                >
+                  Buscar
+                </Button>
+              </Flex>
+            </form>
+            {user && <SuggestedUser user={user} setUser={setUser} />}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
