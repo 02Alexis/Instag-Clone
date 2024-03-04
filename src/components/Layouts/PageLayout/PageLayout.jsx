e3099ab8cca9cc62675d577b6440ba8362431509
02Alexis <m_chael1012@hotmail.com>
2024-03-04 10:32:18 -0500

    uso de 'useAuthState', creacion de navegacion y spinner

--- src/components/Layouts/PageLayout/PageLayout.jsx
+++ src/components/Layouts/PageLayout/PageLayout.jsx
@@ -1,20 +1,31 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/firebase";
 import Sidebar from "../../Sidebar/Sidebar";
+import Navbar from "../../Navbar/Navbar";
 
 // en lugar de agregar el componente Sidebar a cada página, podemos agregarlo solo una vez al componente PageLayout y envolver a los elementos secundarios con él. De esta manera, podemos tener una barra lateral en cada página excepto en AuthPage.
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
+  const [user, loading] = useAuthState(auth);
+  const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
 
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* barra lateral a la izquierda */}
-      {pathname !== "/auth" ? (
+      {canRenderSidebar ? (
         <Box w={{ base: "70px", md: "240px" }}>
           <Sidebar />
         </Box>
       ) : null}
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
       {/* el contenido de la página a la derecha */}
       <Box
         flex={1}
@@ -28,3 +39,16 @@ const PageLayout = ({ children }) => {
 };
 
 export default PageLayout;
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex
+      flexDir="column"
+      h="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Spinner size="xl" />
+    </Flex>
+  );
+};
